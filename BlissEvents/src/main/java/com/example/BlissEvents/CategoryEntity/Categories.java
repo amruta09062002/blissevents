5c9b12cd9a09165279f046a511937b5ebd1268de
ADMIN <ADMIN@AKASH>
2024-07-11 19:38:09 +0530

    change

--- BlissEvents/src/main/java/com/example/BlissEvents/CategoryEntity/Categories.java
+++ BlissEvents/src/main/java/com/example/BlissEvents/CategoryEntity/Categories.java
@@ -1,13 +1,18 @@
 package com.example.BlissEvents.CategoryEntity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Categories {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	long categoryId;
 	String categoryName;
 	
 	
-
+ 
 }
