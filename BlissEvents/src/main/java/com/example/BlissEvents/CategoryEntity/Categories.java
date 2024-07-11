541f09a9c46c36d3f007d14a5852a46b509fdc18
ADMIN <ADMIN@AKASH>
2024-07-11 17:58:02 +0530

    add sound,Category and decorations

--- BlissEvents/src/main/java/com/example/BlissEvents/CategoryEntity/Categories.java
+++ BlissEvents/src/main/java/com/example/BlissEvents/CategoryEntity/Categories.java
@@ -1,6 +1,13 @@
 package com.example.BlissEvents.CategoryEntity;
 
+import javax.persistence.Entity;
 
+@Entity
 public class Categories {
+	
+	long categoryId;
+	String categoryName;
+	
+	
 
 }
--- BlissEvents/src/main/java/com/example/BlissEvents/SoundsEntity/Sounds.java
+++ BlissEvents/src/main/java/com/example/BlissEvents/SoundsEntity/Sounds.java
@@ -1,5 +1,78 @@
 package com.example.BlissEvents.SoundsEntity;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.example.BlissEvents.EventsEntity.Events;
+
+@Entity
 public class Sounds {
 
+	private long soundId;
+	private String soundType;
+	private String soundBrand;
+	private int soundQuantity;
+	
+	@ManyToOne(cascade =CascadeType.ALL)
+	@JoinColumn(name = "event_id")
+	private Events events;
+	
+
+	public Sounds() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Sounds( String soundType, String soundBrand, int soundQuantity, Events events) {
+		super();
+		this.soundType = soundType;
+		this.soundBrand = soundBrand;
+		this.soundQuantity = soundQuantity;
+		this.events = events;
+	}
+
+	public long getSoundId() {
+		return soundId;
+	}
+
+	public void setSoundId(long soundId) {
+		this.soundId = soundId;
+	}
+
+	public String getSoundType() {
+		return soundType;
+	}
+
+	public void setSoundType(String soundType) {
+		this.soundType = soundType;
+	}
+
+	public String getSoundBrand() {
+		return soundBrand;
+	}
+
+	public void setSoundBrand(String soundBrand) {
+		this.soundBrand = soundBrand;
+	}
+
+	public int getSoundQuantity() {
+		return soundQuantity;
+	}
+
+	public void setSoundQuantity(int soundQuantity) {
+		this.soundQuantity = soundQuantity;
+	}
+
+	public Events getEvents() {
+		return events;
+	}
+
+	public void setEvents(Events events) {
+		this.events = events;
+	}
+	
+	
+	
 }
