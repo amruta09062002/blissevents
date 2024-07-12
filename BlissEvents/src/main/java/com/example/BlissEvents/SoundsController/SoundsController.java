4ebf6d370f009edda94f7eadb76928ab3e9de147
Sapura Sande <sandesapura@gmail.com>
2024-07-12 15:02:03 +0530

    push

--- BlissEvents/src/main/java/com/example/BlissEvents/SoundsController/SoundsController.java
+++ BlissEvents/src/main/java/com/example/BlissEvents/SoundsController/SoundsController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.example.BlissEvents.SoundsEntity.Sounds;
+
 import com.example.BlissEvents.SoundsService.SoundsService;
 
 @RestController
