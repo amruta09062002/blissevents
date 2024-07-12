004c8f6c98f095e1502c9664b2b4a5a0621b593e
DELL <DELL@DESKTOP-8G12G3A>
2024-07-12 09:32:57 +0530

    work on users

--- BlissEvents/src/main/java/com/example/BlissEvents/UsersController/UsersController.java
+++ BlissEvents/src/main/java/com/example/BlissEvents/UsersController/UsersController.java
@@ -1,14 +1,24 @@
 package com.example.BlissEvents.UsersController;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.BlissEvents.UsersEntity.Users;
 import com.example.BlissEvents.UsersService.UsersService;
 
 @RestController
+@RequestMapping("/api/Users")
 public class UsersController {
 	
 	@Autowired
 	private UsersService userservice;
+	
+	@PostMapping("/insert")
+	public void insertUser(@RequestBody Users user) {
+		userservice.insertUser(user);
+	}
 
 }
--- BlissEvents/src/main/java/com/example/BlissEvents/UsersEntity/Users.java
+++ BlissEvents/src/main/java/com/example/BlissEvents/UsersEntity/Users.java
@@ -1,9 +1,15 @@
 package com.example.BlissEvents.UsersEntity;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.example.BlissEvents.EventsEntity.Events;
+import com.example.BlissEvents.OrganizersEntity.Organizers;
 
 @Entity
 public class Users {
@@ -17,5 +23,95 @@ public class Users {
 	private String FirstName;
 	private String LastName;
 	
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name  = "EventID")
+	Events event;
+	
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "organizerID")
+	Organizers organizer;
+	
+	public Users() {
+		
+	}
 
+	public Users(long userId, String userName, String password, String email, String firstName, String lastName,
+			Events event, Organizers organizer) {
+		super();
+		UserId = userId;
+		UserName = userName;
+		Password = password;
+		this.email = email;
+		FirstName = firstName;
+		LastName = lastName;
+		this.event = event;
+		this.organizer = organizer;
+	}
+
+	public long getUserId() {
+		return UserId;
+	}
+
+	public void setUserId(long userId) {
+		UserId = userId;
+	}
+
+	public String getUserName() {
+		return UserName;
+	}
+
+	public void setUserName(String userName) {
+		UserName = userName;
+	}
+
+	public String getPassword() {
+		return Password;
+	}
+
+	public void setPassword(String password) {
+		Password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFirstName() {
+		return FirstName;
+	}
+
+	public void setFirstName(String firstName) {
+		FirstName = firstName;
+	}
+
+	public String getLastName() {
+		return LastName;
+	}
+
+	public void setLastName(String lastName) {
+		LastName = lastName;
+	}
+
+	public Events getEvent() {
+		return event;
+	}
+
+	public void setEvent(Events event) {
+		this.event = event;
+	}
+
+	public Organizers getOrganizer() {
+		return organizer;
+	}
+
+	public void setOrganizer(Organizers organizer) {
+		this.organizer = organizer;
+	}
+	
+	
+	
 }
