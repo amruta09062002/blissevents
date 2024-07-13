841342f00a7131c2191ab7f53b77563d31d340e1
DELL <DELL@DESKTOP-8G12G3A>
2024-07-13 11:16:33 +0530

    .

--- BlissEvents/src/main/java/com/example/BlissEvents/VenuesEntity/Venues.java
+++ BlissEvents/src/main/java/com/example/BlissEvents/VenuesEntity/Venues.java
@@ -11,11 +11,7 @@ public class Venues {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-<<<<<<< HEAD
 	private Long venueID;
-=======
-	private Long venueId;
->>>>>>> refs/remotes/origin/Akash
 	private String venueName;
 	private String location;
 	private int capacity;
@@ -23,33 +19,17 @@ public class Venues {
 	public Venues() {
 		
 	}
-
-<<<<<<< HEAD
-	public Venues(Long venueID, String venueName, String location, int capacity) {
-		super();
-		this.venueID = venueID;
-=======
+	
 	public Venues(String venueName, String location, int capacity) {
-		super();
->>>>>>> refs/remotes/origin/Akash
 		this.venueName = venueName;
 		this.location = location;
 		this.capacity = capacity;
 	}
-
-<<<<<<< HEAD
-	public Long getVenueID() {
-		return venueID;
-=======
+	
 	public Long getVenueId() {
 		return venueId;
->>>>>>> refs/remotes/origin/Akash
 	}
-
-<<<<<<< HEAD
-	public void setVenueID(Long venueID) {
-		this.venueID = venueID;
-=======
+	
 	public void setVenueId(Long venueId) {
 		this.venueId = venueId;
 >>>>>>> refs/remotes/origin/Akash
